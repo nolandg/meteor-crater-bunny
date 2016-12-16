e56cffaa101f3fcb8a2ecc5cedeaf01d33a4c88f
Noland <noland.germain@gmail.com>
2016-12-15 19:20:18 -0800

    bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 Package.describe({
   name: 'noland:crater-bunny',
-  version: '0.1.7',
+  version: '0.1.8',
   summary: 'Make landing pages super fast with SSR, async, inline, and auto-cleaned CSS',
   git: 'https://github.com/nolandg/meteor-crater-bunny',
   documentation: 'README.md',
