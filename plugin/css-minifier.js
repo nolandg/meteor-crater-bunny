a52d775acb2bbabce171ab760d6f9ea1d1595511
Noland <noland.germain@gmail.com>
2017-01-02 12:42:15 -0800

    simplifying

--- plugin/css-minifier.js
+++ plugin/css-minifier.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-var, no-underscore-dangle, vars-on-top, func-names, prefer-arrow-callback */
 var PostCss = require('./juliancwirko_meteor-PostCss');
 var CssStripper = require('./CssStripper');
-var paths = require('../lib/global-paths');
 
 function CssMinifier() {}
 
